83bf4b84f2b056acfcb3c83ff378c0c320692866
Doner <Doner@DONER->
2017-04-03 01:35:40 +0600

    maximus

--- ConsoleApplication10/Program.cs
+++ ConsoleApplication10/Program.cs
@@ -4,12 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication10
+namespace DelegateSample
 {
+    public delegate void PrintDelegate(int a, int b);
+
+
+    class SuperClass
+    {
+        int a;
+        int b;
+      
+        public void PrintOK(int a, int b)
+        {
+            Console.WriteLine(a + 2*b);
+
+        }
+    }
+
+
+    class SimpleClass
+    {
+        public int a;
+        public int b;
+        public PrintDelegate pd;
+        public void PrintOK()
+        {
+            pd.Invoke(this.a, this.b);
+           
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+
+            SuperClass c = new SuperClass();
+            SimpleClass s = new SimpleClass();
+            s.a = 6;
+            s.b = 3;
+            s.pd = c.PrintOK;
+            s.PrintOK();
         }
     }
 }
